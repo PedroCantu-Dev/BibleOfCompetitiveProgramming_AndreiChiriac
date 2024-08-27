825034bf8841f378677471a6e22caa8120286518
PedroCantu <pedro.cantu_dev@outlook.com>
2024-08-27 09:44:06 -0600

    Jesus made another one

--- 1_Prime_numbers/main.cpp
+++ 1_Prime_numbers/main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 bool isPrime(int n){
+    if(n < 2){
+        return false;
+    }
     for(int i = 2; i<n ; i++){
         if(n % i == 0){
             return false;
@@ -11,7 +14,28 @@ bool isPrime(int n){
     return true; 
 }
 
+#include <iostream>
+
 int main() {
-    cout << isPrime(7) << endl;
+    int n;
+    char choice;
+
+    do {
+        std::cout << "Enter a number: ";
+        std::cin >> n;
+
+        if(isPrime(n)){
+            std::cout << n << " is a prime number." << std::endl;
+        } else {
+            std::cout << n << " is not a prime number." << std::endl;
+        }
+
+
+        std::cout << "Do you want to evaluate another number? (y/n): ";
+        std::cin.get(); // This is to consume the newline character left in the buffer by std::cin >>
+        std::cin.get(choice); // This reads the actual choice character
+
+    } while (choice == 'y' || choice == 'Y');
+
     return 0;
 }
